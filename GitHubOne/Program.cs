1d97efa6dee229e60eaefc8e3ebb7a3f0eca9de9
KrisherSwage <89964564+KrisherSwage@users.noreply.github.com>
2021-09-02 13:40:45 +0500

    Добавление первой версии программы
    Вывод на экран суммы и разности чисел, введенных пользователем, а так же среднее арифметическое их модулей

---
+++ GitHubOne/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            while (true) //цикл проверки верного ввода первого числа
+            {
+                Console.WriteLine("Введите первое целое число");
+                if (int.TryParse(Console.ReadLine(), out int x)) //условие проверки
+                {
+                    a = x;
+                    break;
+                }
+            }
+            
+            while (true) //цикл проверки верного ввода второго числа
+            {
+                Console.WriteLine("Введите второе целое число");
+                if (int.TryParse(Console.ReadLine(), out int y)) //условие проверки
+                {
+                    b = y;
+                    break;
+                }
+            }
+            
+            int sum, dif;
+            sum = a + b;
+            dif = a - b;
+
+            double mean;
+            if (a < 0)
+            {
+                a = a*(-1);
+
+            }
+            if (b < 0)
+            {
+                b = b * (-1);
+
+            }
+            mean = (a + b) / 2;
+
+            Console.WriteLine($"Сумма введенных чисел равна {sum}");
+            Console.WriteLine($"Разность введенных чисел равна {dif}");
+            Console.WriteLine($"Среднее арифметическое модулей введенных чисел равно {mean}");
+
+            Console.ReadLine(); // чтобы консоль не закрывалась автоматически
+        }
+    }
+}
